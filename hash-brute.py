b866176c7fe8fc11c53f0144635202d8ec2d8cab
Donald Mohlala <137010579+ByteBeem@users.noreply.github.com>
2023-08-19 23:51:05 +0200

    Update hash-brute.py

--- hash-brute.py
+++ hash-brute.py
@@ -14,102 +14,102 @@ def worker(args):
 
             # Calculate the hash for each word in the wordlist
     hash_result = None
-            if hash_type == 1:
-                hash_result = hashlib.md2(line.encode()).hexdigest()
-            elif hash_type == 2:
-                 hash_result = MD4.new(line.encode()).hexdigest()
-            elif hash_type == 3:
-                hash_result = hashlib.md5(line.encode()).hexdigest()
-            elif hash_type == 4:
-                hash_result = hashlib.sha1(line.encode()).hexdigest()
-            elif hash_type == 5:
-                hash_result = hashlib.sha224(line.encode()).hexdigest()
-            elif hash_type == 6:
-                hash_result = hashlib.sha256(line.encode()).hexdigest()
-            elif hash_type == 7:
-                hash_result = hashlib.sha384(line.encode()).hexdigest()
-            elif hash_type == 8:
-                hash_result = hashlib.sha512(line.encode()).hexdigest()
-            elif hash_type == 9:
-                hash_result = hashlib.sha3_224(line.encode()).hexdigest()
-            elif hash_type == 10:
-                hash_result = hashlib.sha3_256(line.encode()).hexdigest()
-            elif hash_type == 11:
-                hash_result = hashlib.sha3_384(line.encode()).hexdigest()
-            elif hash_type == 12:
-                hash_result = hashlib.sha3_512(line.encode()).hexdigest()
-            elif hash_type == 13:
-                hash_result = hex(zlib.crc32(line.encode()))[2:]  # Remove '0x' prefix
-            elif hash_type == 14:
-                hash_result = hex(zlib.crc32(line.encode(), zlib.crc32c))[2:]  # Remove '0x' prefix
-            elif hash_type == 15:
-                hash_result = hex(zlib.crc64(line.encode()))[2:]  # Remove '0x' prefix
-            elif hash_type == 16:
-                hash_result = hashlib.new('whirlpool', line.encode()).hexdigest()
-            elif hash_type == 17:
-                hash_result = hashlib.blake2s(line.encode()).hexdigest()
-            elif hash_type == 18:
-                hash_result = hashlib.new('ripemd128', line.encode()).hexdigest()
-            elif hash_type == 19:
-                hash_result = hashlib.new('ripemd160', line.encode()).hexdigest()
-            elif hash_type == 20:
-                hash_result = hashlib.new('ripemd256', line.encode()).hexdigest()
-            elif hash_type == 21:
-                hash_result = hashlib.new('ripemd320', line.encode()).hexdigest()
-            elif hash_type == 22:
-                hash_result = hashlib.new('tiger192,3', line.encode()).hexdigest()
-            elif hash_type == 23:
-                hash_result = hashlib.new('skein256', line.encode()).hexdigest()
-            elif hash_type == 24:
-                hash_result = hashlib.new('skein512', line.encode()).hexdigest()
-            elif hash_type == 25:
-                hash_result = hashlib.new('gost94', line.encode()).hexdigest()
-            elif hash_type == 26:
-                hash_result = hex(zlib.adler32(line.encode()))[2:]  # Remove '0x' prefix
-            elif hash_type == 27:
-                hash_result = hex(zlib.fletcher32(line.encode()))[2:]  # Remove '0x' prefix
-            elif hash_type == 28:
-                hash_result = hex(hashlib.fnv1_32(line.encode()))[2:]  # Remove '0x' prefix
-            elif hash_type == 29:
-                hash_result = hex(hashlib.fnv1a_32(line.encode()))[2:]  # Remove '0x' prefix
-            elif hash_type == 30:
-                hash_result = hex(hashlib.murmurhash2_32(line.encode()))[2:]  # Remove '0x' prefix
-            elif hash_type == 31:
-                hash_result = hex(hashlib.murmurhash3_32(line.encode()))[2:]  # Remove '0x' prefix
-            elif hash_type == 32:
-                hash_result = hex(hashlib.cityhash.cityhash32(line.encode()))[2:]  # Remove '0x' prefix
-            elif hash_type == 33:
-                hash_result = hex(hashlib.cityhash.cityhash64(line.encode()))[2:]  # Remove '0x' prefix
-            elif hash_type == 34:
-                hash_result = hex(hashlib.xxhash.xxh32(line.encode()))[2:]  # Remove '0x' prefix
-            elif hash_type == 35:
-                hash_result = hex(hashlib.xxhash.xxh64(line.encode()))[2:]  # Remove '0x' prefix
-            elif hash_type == 36:
-                hash_result = hex(hashlib.jenkins(line.encode()))[2:]  # Remove '0x' prefix
-            elif hash_type == 37:
-                hash_result = hex(hashlib.siphash24(line.encode(), 0, 0))[2:]  # Remove '0x' prefix
-            elif hash_type == 38:
-                hash_result = hashlib.new('pearson', line.encode()).hexdigest()
-            elif hash_type == 39:
-                hash_result = hex(hashlib.elf(line.encode()))[2:]  # Remove '0x' prefix
-            elif hash_type == 40:
-                hash_result = hex(hashlib.jenkins(line.encode(), 0))[2:]  # Remove '0x' prefix
-            elif hash_type == 41:
-                hash_result = hex(hashlib.spooky_32(line.encode()))[2:]  # Remove '0x' prefix
-            elif hash_type == 42:
-                hash_result = hex(hashlib.farmhash.fingerprint32(line.encode()))[2:]  # Remove '0x' prefix
-            elif hash_type == 43:
-                hash_result = hex(hashlib.farmhash.fingerprint64(line.encode()))[2:]  # Remove '0x' prefix
-            elif hash_type == 44:
-                hash_result = hex(hashlib.hdlc(line.encode()))[2:]  # Remove '0x' prefix
-            elif hash_type == 45:
-                # bcrypt password hashing
-                if bcrypt.checkpw(line.encode(), hashed_value.encode()):
-                    print(f"Hash cracked! The original value is: {line}")
-                    print(f"Found hash type: bcrypt (hash type 45)")
-                    return
-                else:
-                    continue
+        if hash_type == 1:
+            hash_result = hashlib.md2(line.encode()).hexdigest()
+        elif hash_type == 2:
+             hash_result = MD4.new(line.encode()).hexdigest()
+        elif hash_type == 3:
+            hash_result = hashlib.md5(line.encode()).hexdigest()
+        elif hash_type == 4:
+            hash_result = hashlib.sha1(line.encode()).hexdigest()
+        elif hash_type == 5:
+            hash_result = hashlib.sha224(line.encode()).hexdigest()
+        elif hash_type == 6:
+            hash_result = hashlib.sha256(line.encode()).hexdigest()
+        elif hash_type == 7:
+            hash_result = hashlib.sha384(line.encode()).hexdigest()
+        elif hash_type == 8:
+            hash_result = hashlib.sha512(line.encode()).hexdigest()
+        elif hash_type == 9:
+            hash_result = hashlib.sha3_224(line.encode()).hexdigest()
+        elif hash_type == 10:
+            hash_result = hashlib.sha3_256(line.encode()).hexdigest()
+        elif hash_type == 11:
+            hash_result = hashlib.sha3_384(line.encode()).hexdigest()
+        elif hash_type == 12:
+            hash_result = hashlib.sha3_512(line.encode()).hexdigest()
+        elif hash_type == 13:
+            hash_result = hex(zlib.crc32(line.encode()))[2:]  # Remove '0x' prefix
+        elif hash_type == 14:
+            hash_result = hex(zlib.crc32(line.encode(), zlib.crc32c))[2:]  # Remove '0x' prefix
+        elif hash_type == 15:
+            hash_result = hex(zlib.crc64(line.encode()))[2:]  # Remove '0x' prefix
+        elif hash_type == 16:
+            hash_result = hashlib.new('whirlpool', line.encode()).hexdigest()
+        elif hash_type == 17:
+            hash_result = hashlib.blake2s(line.encode()).hexdigest()
+        elif hash_type == 18:
+            hash_result = hashlib.new('ripemd128', line.encode()).hexdigest()
+        elif hash_type == 19:
+            hash_result = hashlib.new('ripemd160', line.encode()).hexdigest()
+        elif hash_type == 20:
+            hash_result = hashlib.new('ripemd256', line.encode()).hexdigest()
+        elif hash_type == 21:
+            hash_result = hashlib.new('ripemd320', line.encode()).hexdigest()
+        elif hash_type == 22:
+            hash_result = hashlib.new('tiger192,3', line.encode()).hexdigest()
+        elif hash_type == 23:
+            hash_result = hashlib.new('skein256', line.encode()).hexdigest()
+        elif hash_type == 24:
+            hash_result = hashlib.new('skein512', line.encode()).hexdigest()
+        elif hash_type == 25:
+            hash_result = hashlib.new('gost94', line.encode()).hexdigest()
+        elif hash_type == 26:
+            hash_result = hex(zlib.adler32(line.encode()))[2:]  # Remove '0x' prefix
+        elif hash_type == 27:
+            hash_result = hex(zlib.fletcher32(line.encode()))[2:]  # Remove '0x' prefix
+        elif hash_type == 28:
+            hash_result = hex(hashlib.fnv1_32(line.encode()))[2:]  # Remove '0x' prefix
+        elif hash_type == 29:
+            hash_result = hex(hashlib.fnv1a_32(line.encode()))[2:]  # Remove '0x' prefix
+        elif hash_type == 30:
+            hash_result = hex(hashlib.murmurhash2_32(line.encode()))[2:]  # Remove '0x' prefix
+        elif hash_type == 31:
+            hash_result = hex(hashlib.murmurhash3_32(line.encode()))[2:]  # Remove '0x' prefix
+        elif hash_type == 32:
+            hash_result = hex(hashlib.cityhash.cityhash32(line.encode()))[2:]  # Remove '0x' prefix
+        elif hash_type == 33:
+            hash_result = hex(hashlib.cityhash.cityhash64(line.encode()))[2:]  # Remove '0x' prefix
+        elif hash_type == 34:
+            hash_result = hex(hashlib.xxhash.xxh32(line.encode()))[2:]  # Remove '0x' prefix
+        elif hash_type == 35:
+            hash_result = hex(hashlib.xxhash.xxh64(line.encode()))[2:]  # Remove '0x' prefix
+        elif hash_type == 36:
+            hash_result = hex(hashlib.jenkins(line.encode()))[2:]  # Remove '0x' prefix
+        elif hash_type == 37:
+            hash_result = hex(hashlib.siphash24(line.encode(), 0, 0))[2:]  # Remove '0x' prefix
+        elif hash_type == 38:
+            hash_result = hashlib.new('pearson', line.encode()).hexdigest()
+        elif hash_type == 39:
+            hash_result = hex(hashlib.elf(line.encode()))[2:]  # Remove '0x' prefix
+        elif hash_type == 40:
+            hash_result = hex(hashlib.jenkins(line.encode(), 0))[2:]  # Remove '0x' prefix
+        elif hash_type == 41:
+            hash_result = hex(hashlib.spooky_32(line.encode()))[2:]  # Remove '0x' prefix
+        elif hash_type == 42:
+            hash_result = hex(hashlib.farmhash.fingerprint32(line.encode()))[2:]  # Remove '0x' prefix
+        elif hash_type == 43:
+            hash_result = hex(hashlib.farmhash.fingerprint64(line.encode()))[2:]  # Remove '0x' prefix
+        elif hash_type == 44:
+            hash_result = hex(hashlib.hdlc(line.encode()))[2:]  # Remove '0x' prefix
+        elif hash_type == 45:
+            # bcrypt password hashing
+            if bcrypt.checkpw(line.encode(), hashed_value.encode()):
+                print(f"Hash cracked! The original value is: {line}")
+                print(f"Found hash type: bcrypt (hash type 45)")
+                return
+            else:
+                continue
             elif hash_type == 46:
                 # Argon2 password hashing
                 argon2_hash = passlib_argon2.identify(hashed_value)
